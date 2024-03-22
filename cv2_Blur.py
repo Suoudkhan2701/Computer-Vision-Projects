87c839da666a34ac8b5fea4515ba75984a20bc0b
Suoudkhan2701 <131143075+Suoudkhan2701@users.noreply.github.com>
2024-03-22 21:28:36 +0400

    Delete cv2_Blur.py

--- cv2_Blur.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-img=cv2.imread(r"C:\Users\suoud\OneDrive\Documents\flower.jpg")
-cv2.imshow("Picture",img)
-a=np.zeros((5,5),np.uint8)
-ar=cv2.erode(img,a)
-cv2.imshow("Erode",ar)
-if cv2.waitKey(0) & 0xFF == ord("q"):
-   cv2.destroyWindow()
